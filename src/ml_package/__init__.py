f9076ce7a7ae8a08ce030eceddb108e936dec9f9
prsna123 <prasanna.babu@tigeranalytics.com>
2025-01-30 11:07:50 +0000

    fixing the init py filr

--- src/ml_package/__init__.py
+++ src/ml_package/__init__.py
@@ -1,17 +1,12 @@
-from .data_ingestion import fetch_and_load_data
-from .scoring import evaluate_model
-from .training import train_model
+from .data_ingestion import fetch_housing_data, load_housing_data
+from .data_preprocessing import preprocess_data, stratified_split
+from .training import train_linear_regression, train_random_forest
 
-
-def run_pipeline():
-    print("Fetching and loading data...")
-    housing, strat_train_set, strat_test_set = fetch_and_load_data()
-
-    print("Training the model...")
-    final_model, imputer = train_model(strat_train_set)
-
-    print("Evaluating the model...")
-    final_rmse = evaluate_model(final_model, imputer, strat_test_set)
-
-    print(f"Final RMSE on Test Data: {final_rmse}")
-    return final_rmse
+__all__ = [
+    "fetch_housing_data",
+    "load_housing_data",
+    "stratified_split",
+    "preprocess_data",
+    "train_linear_regression",
+    "train_random_forest",
+]
